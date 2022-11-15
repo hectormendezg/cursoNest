1a54260d27f23247afeaeacffc351c06894cc5dd
Hector Mendez <h.mendez@grupofirma.cl>
2022-11-15 11:38:57 -0300

    change entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -11,12 +11,7 @@ export class User {
   @Column()
   password: string;
 
-  @Column({
-    type: 'datetime',
-    default: () => {
-      'CURRENT_TIMESTAMP';
-    },
-  })
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
   @Column({ nullable: true })
